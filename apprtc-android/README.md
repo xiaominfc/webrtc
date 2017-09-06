275b4d3ab8740f3762e79beaa9dbef98c0f11283
xiaominfc@126.com <793101503@qq.com>
2017-09-06 16:31:45 +0800

    upload android client

---
+++ apprtc-android/README.md
@@ -0,0 +1,4 @@
+# AppRTCDemo
+
+Android Studio project for AppRTCDemo of WebRTC project. The revision number of this build is 15663.
+https://chromium.googlesource.com/external/webrtc/+/b4ad603b47f269626585cca717cc53c7e944a8c4
---
+++ apprtc-android/app/src/main/java/com/xiaominfc/apprtc/RTCApplication.java
@@ -0,0 +1,14 @@
+package com.xiaominfc.apprtc;
+
+import android.app.Application;
+
+/**
+ * Created by xiaominfc on 07/08/2017.
+ */
+
+public class RTCApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
