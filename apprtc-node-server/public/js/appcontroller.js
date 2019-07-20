dca4cb1fa0f347e517956996e5fda0f2068b69c5
xiaominfc <xiaominfc@126.com>
2019-07-20 11:26:51 +0800

    fix bug

--- apprtc-node-server/public/js/appcontroller.js
+++ apprtc-node-server/public/js/appcontroller.js
@@ -407,9 +407,9 @@ AppController.prototype.onKeyPress_ = function(event) {
 
 AppController.prototype.pushCallNavigation_ = function(roomId, roomLink) {
   if (!isChromeApp()) {
-    window.history.pushState({'roomId': roomId, 'roomLink': roomLink},
-                             roomId,
-                             roomLink);
+    // window.history.pushState({'roomId': roomId, 'roomLink': roomLink},
+    //                          roomId,
+    //                          roomLink);
   }
 };
 
