9aa791ff118b8184b5381dd03f69227e5273fdf9
xiaominfc <xiaominfc@126.com>
2019-07-20 17:20:03 +0800

    update code

--- apprtc-node-server/public/js_src/appwindow.js
+++ apprtc-node-server/public/js_src/appwindow.js
@@ -13,7 +13,7 @@
 'use strict';
 
 // Generate random room id and connect.
-var roomServer = 'https://appr.tc';
+var roomServer = 'https://apprtc.xiaominfc.com';
 var loadingParams = {
   errorMessages: [],
   warningMessages: [],
