3f8ada1c2f7d84aa7636f14b2465101e45b1ff35
xiaominfc <xiaominfc@126.com>
2019-07-22 16:15:58 +0800

    update code

--- apprtc-node-server/routes/index.js
+++ apprtc-node-server/routes/index.js
@@ -449,11 +449,11 @@ router.post('/message/:roomId/:clientId', function(req, res, next) {
 
 
 router.get('/turn', function(req, res, next) {
-  res.send({iceServers:[{urls:['turn:apprtc.xiaominfc.com:3478?transport=udp','turn:xiaominfc.com:3478'],username:'rtc',credential:'rtc'}]});
+  res.send({iceServers:[{urls:['turn:apprtc.xiaominfc.com:3478','turn:xiaominfc.com:3478'],username:'rtc',credential:'rtc'},{urls:['stun:apprtc.xiaominfc.com:3478']}]});
 });
 
 router.post('/turn', function(req, res, next) {
-  res.send({iceServers:[{urls:['turn:apprtc.xiaominfc.com:3478?transport=udp','turn:xiaominfc.com:3478?transport=tcp'],username:'rtc',credential:'rtc'}]});
+  res.send({iceServers:[{urls:['turn:apprtc.xiaominfc.com:3478','turn:xiaominfc.com:3478'],username:'rtc',credential:'rtc'},{urls:['stun:apprtc.xiaominfc.com:3478']}]});
 });
 
 router.get('/r/:roomId', function(req, res, next) {
