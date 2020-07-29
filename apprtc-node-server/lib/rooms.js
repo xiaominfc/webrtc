a1b4f0937de765b6807c31b0a745df2ffd208736
xiaominfc <xiaominfc@126.com>
2020-07-29 11:57:37 +0800

    format code

--- apprtc-node-server/lib/rooms.js
+++ apprtc-node-server/lib/rooms.js
@@ -5,22 +5,22 @@
  *  the chat
  * @constructor
  */
-Client = function(isInitiator) {
-  var self = this;
-  this.isInitiator = isInitiator;
-  this.messages = [];
+Client = function (isInitiator) {
+	var self = this;
+	this.isInitiator = isInitiator;
+	this.messages = [];
 
-  this.addMessage = function(message) {
-    self.messages.push(message);
-  };
+	this.addMessage = function (message) {
+		self.messages.push(message);
+	};
 
-  this.clearMessages = function() {
-    self.messages = [];
-  };
+	this.clearMessages = function () {
+		self.messages = [];
+	};
 
-  this.toString = function() {
-    return '{ '+ self.isInitiator +', '+ self.messages.length +' }';
-  }
+	this.toString = function () {
+		return '{ ' + self.isInitiator + ', ' + self.messages.length + ' }';
+	}
 }
 
 /**
@@ -28,42 +28,42 @@ Client = function(isInitiator) {
  * the 2 users in a chat.
  * @constructor
  */
-Room = function() {
-  var self = this;
-  var clientMap = {}; //map of key/value pair for client objects
+Room = function () {
+	var self = this;
+	var clientMap = {}; //map of key/value pair for client objects
 
-  this.getOccupancy = function() {
-    var keys = Object.keys(clientMap);
-    return keys.length;
-  };
+	this.getOccupancy = function () {
+		var keys = Object.keys(clientMap);
+		return keys.length;
+	};
 
-  this.hasClient = function(clientId) {
-    return clientMap[clientId];
-  }
+	this.hasClient = function (clientId) {
+		return clientMap[clientId];
+	}
 
-  this.join = function(clientId, callback) {
-    var clientIds = Object.keys(clientMap);
-    var otherClient = clientIds.length > 0 ? clientMap[clientIds[0]] : null;
-    var isInitiator = otherClient == null;
-    var client = new Client(isInitiator);
-    clientMap[clientId] = client;
-    if (callback) callback(null, client, otherClient);
-  };
+	this.join = function (clientId, callback) {
+		var clientIds = Object.keys(clientMap);
+		var otherClient = clientIds.length > 0 ? clientMap[clientIds[0]] : null;
+		var isInitiator = otherClient == null;
+		var client = new Client(isInitiator);
+		clientMap[clientId] = client;
+		if (callback) callback(null, client, otherClient);
+	};
 
-  this.removeClient = function(clientId, callback) {
-    delete clientMap[clientId];
-    var clientIds = Object.keys(clientMap);
-    var otherClient = clientIds.length > 0 ? clientMap[clientIds[0]] : null;
-    callback(null, true, otherClient);
-  };
+	this.removeClient = function (clientId, callback) {
+		delete clientMap[clientId];
+		var clientIds = Object.keys(clientMap);
+		var otherClient = clientIds.length > 0 ? clientMap[clientIds[0]] : null;
+		callback(null, true, otherClient);
+	};
 
-  this.getClient = function(clientId) {
-    return clientMap[clientId];
-  }
+	this.getClient = function (clientId) {
+		return clientMap[clientId];
+	}
 
-  this.toString = function() {
-    return JSON.stringify(Object.keys(clientMap));
-  };
+	this.toString = function () {
+		return JSON.stringify(Object.keys(clientMap));
+	};
 };
 
 /**
@@ -73,34 +73,34 @@ Room = function() {
  * concurrent video chat sessions.
  * @constructor
  */
-Rooms = function() {
-  var self = this;
-  var roomMap = {}; //map of key/value pair for room objects
+Rooms = function () {
+	var self = this;
+	var roomMap = {}; //map of key/value pair for room objects
 
-  this.get = function(roomCacheKey, callback) {
-    var room = roomMap[roomCacheKey];
-    callback(null, room);
-  };
+	this.get = function (roomCacheKey, callback) {
+		var room = roomMap[roomCacheKey];
+		callback(null, room);
+	};
 
-  this.create = function(roomCacheKey, callback) {
-    var room = new Room;
-    roomMap[roomCacheKey] = room;
-    callback(null, room);
-  };
+	this.create = function (roomCacheKey, callback) {
+		var room = new Room;
+		roomMap[roomCacheKey] = room;
+		callback(null, room);
+	};
 
-  this.createIfNotExist = function(roomCacheKey, callback) {
-    self.get(roomCacheKey, function(error, room) {
-      if (error) {
-        callback(error);
-        return;
-      }
-      if (!room) {
-        self.create(roomCacheKey, callback);
-      } else {
-        callback(error, room);
-      }
-    });
-  }
+	this.createIfNotExist = function (roomCacheKey, callback) {
+		self.get(roomCacheKey, function (error, room) {
+			if (error) {
+				callback(error);
+				return;
+			}
+			if (!room) {
+				self.create(roomCacheKey, callback);
+			} else {
+				callback(error, room);
+			}
+		});
+	}
 };
 
 module.exports = Rooms;
